b1e86188014288b016a87ac1d265469010f0caf4
Domen - privat <domen-privat@MacBook-Pro.local>
2015-09-03 23:50:56 +0200

    add notifications test

---
+++ src/Mimazoo/SoaBundle/Command/NotificationFeedbackCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Mimazoo\SoaBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Mimazoo\SoaBundle\Entity\User;
+use Mimazoo\SoaBundle\ValueObject\TimeRange;
+use Mimazoo\SoaBundle\ValueObject\TimeRangeCommand;
+use Mimazoo\SoaBundle\ValueObject\DailyDateTimeInterval;
+use Mimazoo\SoaBundle\ValueObject\YearlyDateTimeInterval;
+use RMS\PushNotificationsBundle\Message\iOSMessage;
+
+use Mimazoo\SoaBundle\Model\Schedule\Schedule;
+
+
+class NotificationFeedbackCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('notifications:feedback')
+            ->setDescription('Receive notification feedback')
+        ;
+    }
+    
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+    	
+        $feedbackService = $this->getContainer()->get("rms_push_notifications.ios.feedback");
+        $uuids = $feedbackService->getDeviceUUIDs();
+
+        foreach ($uuids as $uuid) {
+            print_r($uuid);
+        }
+    }
+    
+}
